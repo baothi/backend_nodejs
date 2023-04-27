341fc5ad9e9f4526f00614bf5fb5aef155ac43c1
baothi <baothi246@gmail.com>
2023-04-27 09:08:38 +0700

    check connection database

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 // get the client
 const mysql = require('mysql2/promise');
 
@@ -11,17 +12,49 @@ const mysql = require('mysql2/promise');
 //   database: process.env.DB_NAME,
 // });
 
-const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  queueLimit: 0
-});
+// const connection = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+//   idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+//   queueLimit: 0
+// });
+
+
+var dbState = [{
+  value: 0,
+  label: "disconnected"
+},
+{
+  value: 1,
+  label: "connected"
+},
+{
+  value: 2,
+  label: "connecting"
+},
+{
+  value: 3,
+  label: "disconnecting"
+}];
+
+
+
+const connection = async () => {
+  //or
+  try {
+    await mongoose.connect('mongodb://root:123456@localhost:27018');
+    const state = Number(mongoose.connection.readyState);
+    console.log(dbState.find(f => f.value == state).label, "to db 1111");
+  } catch (error) {
+    console.log(">>> Error: " + error)
+  }
+}
+
 
 module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -10,44 +10,20 @@ const port = process.env.PORT || 8080;
 const hostname = process.env.HOST_NAME || "localhost";
 
 // config req.body
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // for json
 
 configViewEngine(app);
 
 // khai bao route
-app.use('/',webRoutes)
+app.use('/', webRoutes)
 
-// simple query
-// connection.query(
-//   'SELECT * FROM Users u',
-//   function(err, results, fields) {
-//     console.log(">>> results : ",results); // results contains rows returned by server
-//     // console.log(">>> fields : ",fields); // fields contains extra meta data about results, if available
-//   }
-// );
-
+// test connection
+connection();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
 
 
-// npm install --save-exact express@4.18.2
-// npm install --save-exact ejs@3.1.8
-// npm install --save-dev nodemon@2.0.20
-// npm install --save-exact body-parser@1.19.0  @babel/core@7.15.5 @babel/node@7.15.4 @babel/preset-env@7.15.6
-// npm install --save-exact dotenv@16.0.3
-// npm install --save-exact pg-promise@10.12.0
-// npm install --save-exact multer@1.4.3
-// npm install --save-exact app-root-path@3.0.0
-// npm install --save-exact morgan@1.10.0
-// npm install --save-exact mysql2@3.1.2
-
-// CREATE TABLE Users (
-//   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-//   email varchar(255) NOT NULL,
-//   name varchar(255) NOT NULL,
-//   city varchar(255)
-// );
 
