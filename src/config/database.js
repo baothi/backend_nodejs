8642ae086598f154ee8de552116f4ed5ebaacdcb
baothi <baothi246@gmail.com>
2023-04-27 10:25:03 +0700

    connect db

--- src/config/database.js
+++ src/config/database.js
@@ -1,30 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 // get the client
-const mysql = require('mysql2/promise');
-
-// create the connection to database
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-// const connection = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-//   idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-//   queueLimit: 0
-// });
-
+// const mysql = require('mysql2/promise');
 
 var dbState = [{
   value: 0,
@@ -48,7 +25,12 @@ var dbState = [{
 const connection = async () => {
   //or
   try {
-    await mongoose.connect('mongodb://root:123456@localhost:27018');
+    const options = {
+      user: process.env.MONGO_DB_ROOT_USERNAME,
+      pass: process.env.MONGO_DB_ROOT_PASSWORD,
+      dbName: process.env.MONGO_DB_ROOT_DB_NAME,
+    }
+    await mongoose.connect(process.env.MONGO_DB_HOST, options);
     const state = Number(mongoose.connection.readyState);
     console.log(dbState.find(f => f.value == state).label, "to db 1111");
   } catch (error) {
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web');
 const connection = require('./config/database');
-
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 8080;
 const hostname = process.env.HOST_NAME || "localhost";
@@ -16,14 +16,36 @@ app.use(express.json()); // for json
 configViewEngine(app);
 
 // khai bao route
+
+
+// connection();
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// });
+
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+const silence = new Kitten({ name: 'Silence' });
+console.log(silence.name); // 'Silence'
+silence.save();
+
+(async () => {
+  try {
+    await connection();
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    });
+  } catch (error) {
+    console.log(">>>>> Error: " + error)
+  }
+})()
+
 app.use('/', webRoutes)
 
-// test connection
-connection();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
 
 
 
